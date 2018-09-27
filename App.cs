49828cc6e3faef9e3016577a56042a005bd8d88e
Benjamin Jordan <bmj465@gmail.com>
2018-09-27 12:23:53 -0700

    Initial commit.

---
+++ App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateAR.Snap
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Snap Controller
+
+Controls a DSLR camera based on events from Trellis.
+
+
+### Requirements
+
+`gphoto2`
+`dotnet`
